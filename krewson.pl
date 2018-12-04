f7751e3cfd1aa8f6e375500e11beb5eec4182a0c
tannerkrewson <tannerkrewson@gmail.com>
2018-12-03 19:53:12 -0600

    words for first two sentences

---
+++ krewson.pl
@@ -0,0 +1,39 @@
+% Project 1 - Tanner Krewson
+
+% det - Determiner
+
+% n - Noun
+n(anything).
+n(noun).
+
+% nsn - Plural Noun
+nsn(doors).
+
+% pro - Pronoun
+pro(we).
+
+% adj - Adhective
+adj(relaxed).
+adj(late).
+
+% adv - Adverb
+adv(more).
+adv(already).
+
+% qual - Qualifier
+
+% v - Verb
+v(be).
+v(been).
+v(were).
+
+% aux - Auxliary verb
+aux(could).
+aux(have).
+
+% p - Preposition
+p(behind).
+p(those).
+
+% conj - Conjunction
+conj(but).
